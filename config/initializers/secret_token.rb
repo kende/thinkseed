4a89f0636ef2b7de4e518aee777214ebb0c5fa24
kende <kende.j@gmail.com>
2011-08-24 19:02:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thinkseed::Application.config.secret_token = '32bcdc5f8e3cc80c074037be33f5801a32d4a84726d856ca16580bbde9a490b4015acebb138c716ddc0783d7b2cfa5507f2d317de501a800f37030aeced24091'
